c71b51a0920deb5b1475593430bc050022eef26b
VaishnaviBoobesh <vaishnaviboobesh@gmail.com>
2026-01-06 01:21:35 +0100

    fix: modified service mock with file extensions check

--- src/services/mock.service.ts
+++ src/services/mock.service.ts
@@ -1,30 +1,42 @@
 type UrlCheckResult = {
-	exists: boolean;
-	urlType?: "file" | "folder";
+  exists: boolean;
+  urlType?: "file" | "folder";
 };
 
- /**
-  * Mocked API method that checks URL existence 
-  * Assumes, ending with "/" as folder, contains "." as file
-  * @param {string} urlInput - URL value to check 
-  * @returns {Promise<UrlCheckResult>} - A promise with check result
-  */
-export function mockCheckUrl(urlInput: string, signal?: AbortSignal): Promise<UrlCheckResult> {
-	return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-		if (urlInput.endsWith("/")) {
-			return resolve({ exists: true, urlType: "folder" });
-		} else if(urlInput.includes('.')) {
-      return resolve({ exists: true, urlType: 'file'}); 
-    } else {
-      return resolve({ exists: false })
-    }
-  }, 3000);
+const MOCK_FILE_EXTENSIONS = [
+  ".txt",
+  ".json",
+  ".jpg",
+  ".png",
+  ".pdf",
+  ".csv",
+  ".html",
+];
 
-    signal?.addEventListener('abort', () => {
+/**
+ * Mocked API method that checks URL existence
+ * Assumes, ending with "/" as folder, contains "." as file
+ * @param {string} urlInput - URL value to check
+ * @returns {Promise<UrlCheckResult>} - A promise with check result
+ */
+export function mockCheckUrl(
+  urlInput: string,
+  signal?: AbortSignal
+): Promise<UrlCheckResult> {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      if (MOCK_FILE_EXTENSIONS.some((ext) => urlInput.endsWith(ext))) {
+        return resolve({ exists: true, urlType: "file" });
+      }
+      if (urlInput.endsWith("/")) {
+        return resolve({ exists: true, urlType: "folder" });
+      }
+      return resolve({ exists: false });
+    }, 1000);
+
+    signal?.addEventListener("abort", () => {
       clearTimeout(timeout);
       reject(new DOMException("Aborted", "AbortError"));
-    })
-
-	});
+    });
+  });
 }
